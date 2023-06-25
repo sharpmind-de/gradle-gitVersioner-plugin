819863ab92829ad4bc88f4685d9a0ce353251278
Marco Stolze <marco@sharpmind.de>
2023-06-25 20:13:47 +0200

    prepared for publishing to gradle plugin portal

--- publish.sh
+++ publish.sh
@@ -1,10 +1,13 @@
-echo "Publishing to OSSRH"
+#echo "Publishing to OSSRH"
 
-echo "Make sure the following properties are set in ~/.gradle/gradle.properties"
-echo "OSSRH_USERNAME"
-echo "OSSRH_PASSWORD"
-echo "signing.keyId"
-echo "signing.password"
-echo "signing.secretKeyRingFile"
+#echo "Make sure the following properties are set in ~/.gradle/gradle.properties"
+#echo "OSSRH_USERNAME"
+#echo "OSSRH_PASSWORD"
+#echo "signing.keyId"
+#echo "signing.password"
+#echo "signing.secretKeyRingFile"
 
-./gradlew clean build publishAllPublicationsToOSSRHRepository
+#./gradlew clean build publishAllPublicationsToOSSRHRepository
+
+echo "Publishing to Gradle Plugins Repository"
+./gradlew clean build publishPlugins
